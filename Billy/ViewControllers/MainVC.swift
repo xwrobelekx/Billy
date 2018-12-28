a04cffbc3b9aa67068e607ba23194866ab13dfe2
Kamil Wrobel <wrobelk@live.com>
2018-12-27 22:29:29 -0500

    created temporary mainVC to switch between table view and collection view

---
+++ Billy/ViewControllers/MainVC.swift
@@ -0,0 +1,39 @@
+//
+//  MainVC.swift
+//  Billy
+//
+//  Created by Kamil Wrobel on 12/27/18.
+//  Copyright © 2018 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    
+
+}
